9ba4062d036697aba8eb7ff03af7a2bad6a81da2
Xiang Zhang <fancyzhx@gmail.com>
2023-08-08 05:38:58 -0400

    Implemented script to download models from TheBloke

---
+++ download/download_thebloke.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Quantized LLaMa models
+git clone git@hf.co:TheBloke/LLaMa-7B-GGML ../models/llama/LLaMa-7B-GGML
+git clone git@hf.co:TheBloke/LLaMa-13B-GGML ../models/llama/LLaMa-13B-GGML
+git clone git@hf.co:TheBloke/LLaMa-30B-GGML ../models/llama/LLaMa-30B-GGML
+git clone git@hf.co:TheBloke/llama-65B-GGML ../models/llama/llama-65B-GGML
+
+# Quantized Alpaca models
+git clone git@hf.co:TheBloke/Alpaca-Lora-30B-GGML ../models/alpaca/Alpaca-Lora-30B-GGML
+git clone git@hf.co:TheBloke/alpaca-lora-65B-GGML ../models/alpaca/alpaca-lora-65B-GGML
+
+# Quantized LLaMa-2 models
+git clone git@hf.co:TheBloke/Llama-2-7B-GGML ../models/llama-2/Llama-2-7B-GGML
+git clone git@hf.co:TheBloke/Llama-2-13B-GGML ../models/llama-2/Llama-2-13B-GGML
+git clone git@hf.co:TheBloke/Llama-2-70B-GGML ../models/llama-2/Llama-2-70B-GGML
+git clone git@hf.co:TheBloke/Llama-2-7B-Chat-GGML ../models/llama-2/Llama-2-7B-Chat-GGML
+git clone git@hf.co:TheBloke/Llama-2-13B-chat-GGML ../models/llama-2/Llama-2-13B-chat-GGML
+git clone git@hf.co:TheBloke/Llama-2-70B-Chat-GGML ../models/llama-2/Llama-2-70B-Chat-GGML
