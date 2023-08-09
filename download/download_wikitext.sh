0868f32576e3cf64fbe5a910ffed735e9370b373
Xiang Zhang <fancyzhx@gmail.com>
2023-08-09 23:51:27 +0800

    Implemented downloader for wikitext

---
+++ download/download_wikitext.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# WikiText-2 word level
+wget -P ../data/wikitext https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-2-v1.zip
+# WikiText-103 word level
+wget -P ../data/wikitext https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-v1.zip
+# WikiText-2 raw character level
+wget -P ../data/wikitext https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-2-raw-v1.zip
+# WikiText-103 raw character level
+wget -P ../data/wikitext https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-raw-v1.zip
